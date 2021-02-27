5b8b268a2aa2fa17660ecaf1f7590c9807d3c7cc
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-02-27 12:33:13 -0300

    Add project dependencies

---
+++ src/test/java/com/trip/planner/TripPlannerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.trip.planner;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
