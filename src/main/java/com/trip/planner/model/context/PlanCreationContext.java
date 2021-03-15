acdb23b32e8aca822f9a155ca0ce0327de141296
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-03-15 20:14:57 -0300

    Update LocalDate format

--- src/main/java/com/trip/planner/model/context/PlanCreationContext.java
+++ src/main/java/com/trip/planner/model/context/PlanCreationContext.java
@@ -18,10 +18,10 @@ public class PlanCreationContext {
     private String countryName;
 
     @NotNull
-    @JsonFormat(pattern = "MM/dd/yyyy")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate departureDate;
 
     @NotNull
-    @JsonFormat(pattern = "MM/dd/yyyy")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate returnDate;
 }
